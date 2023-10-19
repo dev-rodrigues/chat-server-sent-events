052d80511c23909413af6a1ad807f10b1fd7fe31
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2023-10-19 10:54:15 -0300

    adiciona conexao com eureka

---
+++ src/main/java/br/ufrj/coppetec/servicechat/core/configuration/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package br.ufrj.coppetec.servicechat.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .exposedHeaders("Authorization",
+                        "token",
+                        "geracao",
+                        "application",
+                        "Access-Control-Allow-Methods",
+                        "Access-Control-Allow-Headers",
+                        "Access-Control-Max-Age",
+                        "Access-Control-Request-Headers",
+                        "Access-Control-Request-Method")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
